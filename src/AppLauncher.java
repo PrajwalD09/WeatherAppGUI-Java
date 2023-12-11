11a16c103c58e11e4847b000ca47cb3da6215f20
Prajwal D <153450849+PrajwalD09@users.noreply.github.com>
2023-12-11 11:27:42 +0530

    Update AppLauncher.java
    Removed Unwanted comments

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -4,14 +4,11 @@ public class AppLauncher {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable(){
             @Override
-            public void run(){
+            public void run()
+            {
                 // display our weather app gui
                 new WeatherAppGui().setVisible(true);
-
-//                System.out.println(WeatherApp.getLocationData("Tokyo"));
-
-//                System.out.println(WeatherApp.getCurrentTime());
-
+            
             }
         });
     }
